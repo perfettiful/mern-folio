308a6f4a395e738e0ff8c522f72c45e152391aca
Nate <nathanperfetti@gmail.com>
2021-07-12 00:34:52 -0500

    profile name tag updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -35,7 +35,7 @@ const About = () => {
   return (
     <div className="card shadow mx-auto mt-4">
       <div className="card-body text-center">
-        <img src='{ProfilePic}'/>
+        <img width='100%' className='profileImg' src='https://raw.githubusercontent.com/perfettiful/mern-folio/main/public/NatePerfetti.png'/>
         <h4 className="title">Nathan Perfetti</h4>
         <h5 className="title2 font-italic">Certified Full Stack Web Developer & Educator</h5>
         <p className="card-text">nathanperfetti@gmail.com</p>
