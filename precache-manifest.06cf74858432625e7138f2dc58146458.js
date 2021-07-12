e550f013f1a5b462b555708f0c0766c02ff213a0
Nate <nathanperfetti@gmail.com>
2021-07-11 23:20:19 -0500

    Updates

---
+++ precache-manifest.06cf74858432625e7138f2dc58146458.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "02480a1c3a40c8c06a62",
+    "url": "/mern-folio/static/css/main.a8b959cd.chunk.css"
+  },
+  {
+    "revision": "02480a1c3a40c8c06a62",
+    "url": "/mern-folio/static/js/main.02480a1c.chunk.js"
+  },
+  {
+    "revision": "6b79336f098a550b3364",
+    "url": "/mern-folio/static/js/runtime~main.6b79336f.js"
+  },
+  {
+    "revision": "7e725d9e0807b1c24ae4",
+    "url": "/mern-folio/static/js/2.7e725d9e.chunk.js"
+  },
+  {
+    "revision": "3056bf2f96c40cd0a814b355959a6983",
+    "url": "/mern-folio/static/media/html5.3056bf2f.svg"
+  },
+  {
+    "revision": "b675af6016eed403de51899be75efe59",
+    "url": "/mern-folio/static/media/css3.b675af60.svg"
+  },
+  {
+    "revision": "1f6069572c9380a733700436d2980f8a",
+    "url": "/mern-folio/static/media/javascript.1f606957.svg"
+  },
+  {
+    "revision": "6f58459b59dd6de3de3fe34c0bc52212",
+    "url": "/mern-folio/static/media/jquery.6f58459b.svg"
+  },
+  {
+    "revision": "092e0882358cd09dbe9376b64d0f2a0e",
+    "url": "/mern-folio/static/media/react.092e0882.svg"
+  },
+  {
+    "revision": "27b0eaaafe77b376d8bf159a87fe36aa",
+    "url": "/mern-folio/static/media/mysql.27b0eaaa.svg"
+  },
+  {
+    "revision": "8f2548d5f68478e29f805bff7694d0d9",
+    "url": "/mern-folio/static/media/mongodb.8f2548d5.svg"
+  },
+  {
+    "revision": "df8bf7ad749ccc8ae99516952a8f2878",
+    "url": "/mern-folio/static/media/firebase.df8bf7ad.svg"
+  },
+  {
+    "revision": "4edea5d49bd09fd7b5eda2e56d6ab18b",
+    "url": "/mern-folio/static/media/node-dot-js.4edea5d4.svg"
+  },
+  {
+    "revision": "59c0303dc0d73aadd433bd82c724070c",
+    "url": "/mern-folio/static/media/python.59c0303d.svg"
+  },
+  {
+    "revision": "5185ca3d2747685e10cb781ded478c82",
+    "url": "/mern-folio/static/media/django.5185ca3d.svg"
+  },
+  {
+    "revision": "b73214e3ebad909ef6891be949c39947",
+    "url": "/mern-folio/static/media/git.b73214e3.svg"
+  },
+  {
+    "revision": "9df899bc9284143ef1168f6fd0b2387e",
+    "url": "/mern-folio/index.html"
+  }
+];
