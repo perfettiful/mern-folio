fe4d8d600e54769ed7e557c39575113938459bfd
Nathan Perfetti <nathanperfetti@gmail.com>
2025-10-14 23:13:32 -0500

    Update projects.json import from Body.jsx

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
-import projects from '../projects.json';
 import ContactForm from './ContactForm';
 import About from './About';
 import { Animated } from 'react-animated-css';
