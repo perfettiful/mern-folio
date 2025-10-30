30d15efa41152e5d8d5bd964d7cfb6433674a486
Nathan Perfetti <nathanperfetti@gmail.com>
2025-10-30 12:25:36 -0500

    update linking

--- src/App.js
+++ src/App.js
@@ -10,8 +10,34 @@ class App extends Component {
     currentPage: 'home'
   };
 
+  componentDidMount() {
+    // Check URL parameters on initial load
+    this.checkUrlParams();
+    // Listen for URL changes (back/forward buttons)
+    window.addEventListener('popstate', this.checkUrlParams);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('popstate', this.checkUrlParams);
+  }
+
+  checkUrlParams = () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const page = urlParams.get('page');
+    if (page === 'resume') {
+      this.setState({ currentPage: 'resume' });
+    } else {
+      this.setState({ currentPage: 'home' });
+    }
+  };
+
   setCurrentPage = (page) => {
     this.setState({ currentPage: page });
+    // Update URL without page reload
+    const url = page === 'home' ? 
+      window.location.pathname : 
+      `${window.location.pathname}?page=${page}`;
+    window.history.pushState({}, '', url);
   };
 
   render() {
